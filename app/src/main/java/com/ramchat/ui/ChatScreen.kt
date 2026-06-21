ed1ce9a458abaedefb5983e6b3298fb6d27765f0
soumyajitgiri2008-png <soumyajitgiri2008@gmail.com>
2026-06-21 17:50:19 +0530

    Update ChatScreen.kt

--- app/src/main/java/com/ramchat/ui/ChatScreen.kt
+++ app/src/main/java/com/ramchat/ui/ChatScreen.kt
@@ -8,7 +8,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.*
-import androidx.compose.foundation.layout.fillMaxWidth 
+import androidx.compose.foundation.* 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
